db9e7b20f3797d4ba56d077d882a2bebedd03acc
godsonten <tendongze.g@zsoftly.com>
2025-08-28 00:22:00 +0100

    Revert "updated release date"
    This reverts commit 875c0d43dbe56dbbed9c1f4a4769ec696e521396.

--- src/00_version.sh
+++ src/00_version.sh
@@ -4,5 +4,5 @@
 # This is the single source of truth for version management
 
 export ZTIAWS_VERSION="1.6.0"
-export ZTIAWS_BUILD_DATE="2025-08-27"
+export ZTIAWS_BUILD_DATE="2025-08-19"
 export ZTIAWS_PROJECT_NAME="ztiaws"
